4f2efd968070077f6dbf3724cf5425a953b11e60
Tomer Gingold <tomer.gingold@gmail.com>
2023-07-08 02:20:31 +0300

    added the animated icons

--- main.gd
+++ main.gd
@@ -9,4 +9,10 @@ func _on_quit_pressed():
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://levels.tscn")
+	get_tree().change_scene_to_file("res://level1_screen.tscn")
+
+var color = Color.hex64(0x9A6447)
+var size = Vector2(100, 100)
+
+func _draw():
+	draw_rect(Rect2(0, 0, size.x, size.y), color)
