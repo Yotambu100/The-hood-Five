98b3f75709594e8e304abbd0cfdb651d5f4a0fea
Tomer Gingold <tomer.gingold@gmail.com>
2023-07-08 10:46:47 +0300

    added level 2

--- level_real_1.gd
+++ level_real_1.gd
@@ -1,5 +1,6 @@
 extends Node2D
-var level_actions =["jr", "jr", "r", "r", "r", "j"]
+
+var level_actions =["jr","r", "jr", "r", "r", "r", "j"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func _on_tile_map_ready():
+	pass # Replace with function body.
+	
--- levels.gd
+++ levels.gd
@@ -7,3 +7,7 @@ extends Node2D
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://level_real_1.tscn")
 
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://level_real_2.tscn")
