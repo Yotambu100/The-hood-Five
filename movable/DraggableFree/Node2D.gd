2318991cbb16572686091b9cbb933ffc1294a6b0
yotambu100 <yotambu100@gmail.com>
2023-07-08 06:12:35 +0300

    fix: made the draggable collide

---
+++ movable/DraggableFree/Node2D.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var _area : Area2D = $Area2D
+
+var _dragging : bool = false
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and _dragging:
+		set_global_position(get_global_mouse_position())
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		_dragging = event.pressed
