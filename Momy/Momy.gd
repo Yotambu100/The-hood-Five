1f81af06eb566fc321af9de4f275c51f9ab9f3c4
yotambu100 <yotambu100@gmail.com>
2023-07-08 17:04:42 +0300

    major change

---
+++ Momy/Momy.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+
+
+	
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		var current_level = str(get_tree().get_current_scene().get_path())[-1]
+		var next_level = int(current_level)+1
+		get_tree().change_scene_to_file("res://level_real_" +str(next_level) + ".tscn")
