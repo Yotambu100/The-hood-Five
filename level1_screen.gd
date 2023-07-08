0da2fa8c6a94faab92e9ca0affccb26ca6abc846
Tomer Gingold <tomer.gingold@gmail.com>
2023-07-08 03:40:53 +0300

    added some images

---
+++ level1_screen.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+	
+func _on_show_next_moves_pressed():
+	var text = "fwd --> fwd --> jump --> fwd --> fwd --> jump"
+	var font_path = "res://my_font.ttf"
+	var font_size = 16
+	# Create a label node.
+	var label = Label.new()
+	label.text = text
+
+	# Position the label.
+	label.position = Vector2(100, 100)
+# Draw the label.
+	#label.draw_string(Vector2(100, 100))
+
