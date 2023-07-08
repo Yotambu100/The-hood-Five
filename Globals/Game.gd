b9495d9f8559b34f22df48dfbcacf150ea861e40
Itai Gingold <itai.gingold@gmail.com>
2023-07-08 23:17:43 +0300

    hotfix: fixed level 1 scene

--- Globals/Game.gd
+++ Globals/Game.gd
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 extends Node2D
 
 var level_is_running = false
@@ -6,14 +5,8 @@ var Number_OF_LEVELS=5
 #array to collect number of retries in each level, -2=done, -1 not started 
 #var level_scores = Array[5]
 
-=======
-extends Node
-
-var level_is_running = true
-
 # player stats
 var JUMP_VELOCITY = -400.0
 var JUMP_VELOCITY_SPRING = -1000.0
 
 var cur_jump_velocity = JUMP_VELOCITY
->>>>>>> 27a1ca7d5a66406090926260957b303986b78e96
