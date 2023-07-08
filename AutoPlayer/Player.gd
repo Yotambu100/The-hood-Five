959f2f2bd299f5b284abafa9e0adde323c452973
Itai Gingold <itai.gingold@gmail.com>
2023-07-08 07:03:22 +0300

    WIP: level 1

--- AutoPlayer/Player.gd
+++ AutoPlayer/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 const ACTIONS_DICT = {
