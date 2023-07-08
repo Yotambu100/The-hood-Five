d8133ecbadd6912a1e1c18de0dce884c137e9b36
Tomer Gingold <tomer.gingold@gmail.com>
2023-07-08 15:05:55 +0300

    connected screens 4 and 5

--- levels.gd
+++ levels.gd
@@ -11,3 +11,11 @@ func _on_button_pressed():
 
 func _on_button_2_pressed():
 	get_tree().change_scene_to_file("res://level_real_2.tscn")
+
+
+func _on_button_5_pressed():
+	get_tree().change_scene_to_file("res://level_real_5.tscn")
+
+
+func _on_button_4_pressed():
+	get_tree().change_scene_to_file("res://level_real_4.tscn")
